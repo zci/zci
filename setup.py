ba2276a8a4191a699086c2165f6ed2e0efcaa2c5
Zygmunt Krynicki <zkrynicki@gmail.com>
2013-03-23 14:32:00 +0100

    Add typical boilerplate
    1) COPYING with AGPLv3+
    2) MANIFEST.in, setup.py, setup.cfg
    3) distribute_setup.py for bootstrapping
    4) Empty 'zci' package
    
    Signed-off-by: Zygmunt Krynicki <zkrynicki@gmail.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+
+setup(setup_requires=['d2to1'], d2to1=True)
